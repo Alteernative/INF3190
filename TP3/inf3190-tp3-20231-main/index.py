353892b6ebcb7f19c88ed342c5574c20ba6b4df5
Alteernative <120146237+Alteernative@users.noreply.github.com>
2023-04-11 16:55:20 -0400

    ajout recherche

--- TP3/inf3190-tp3-20231-main/index.py
+++ TP3/inf3190-tp3-20231-main/index.py
@@ -11,10 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import random
+import re
 from flask import Flask
 from flask import render_template
 from flask import g
+from flask import request
+from flask import redirect
 from database import Database
 
 app = Flask(__name__, static_url_path="", static_folder="static")
@@ -34,12 +37,60 @@ def close_connection(exception):
         db.disconnect()
 
 
+def get_liste_animaux():
+    return get_db().get_animaux()
+
+
+def get_animaux_index():
+    animaux = get_liste_animaux()
+    random.shuffle(animaux)
+    animaux = animaux[:5]
+    return animaux
+
+
 @app.route('/')
-def form():
-    # À remplacer par le contenu de votre choix.
-    return render_template('index.html')
+def index():
+    return render_template('index.html', animaux=get_animaux_index())
+
+
+@app.route('/recherche', methods=['POST'])
+def donnees_recherche():
+    resultat = []
+    achercher = request.form['recherche']
+    if len(achercher) == 0:
+        return redirect('/')
+    reg = re.compile(achercher, re.IGNORECASE)
+    for animal in get_db().get_animaux():
+        if re.search(reg, animal.get('nom')) or re.findall(reg, animal.get('espece')) \
+                or re.findall(reg, animal.get('race')) or re.findall(reg, animal.get('description')):
+            resultat.append(animal)
+
+    if len(resultat) == 0:
+        return render_template('animal-introuvable.html', animals=get_liste_animaux()[:2])
+
+    return render_template('recherche.html', resultat=resultat)
+
+
+@app.route('/animal', methods=['POST'])
+def adopter():
+    animal_a_adopter = request.form.get('animal')
+    return redirect('/{}'.format(animal_a_adopter))
+
+
+@app.route('/form')
+def afficher():
+    return render_template('recherche.html')
+
+
+@app.route('/<nom_animal>')
+def adopter_animal(nom_animal):
+
+    for animal in get_liste_animaux():
+        if nom_animal == animal.get('nom'):
+            return render_template('animal.html', animal=animal)
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
-
     app.run(debug=True)
