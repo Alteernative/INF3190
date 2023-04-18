82eae363f6de8b507546c114d3bc6c0493b84165
Alteernative <120146237+Alteernative@users.noreply.github.com>
2023-04-18 14:22:31 -0400

    ajouter code permanent au fichier

--- TP3/inf3190-tp3-20231-main/index.py
+++ TP3/inf3190-tp3-20231-main/index.py
@@ -1,4 +1,4 @@
-# Copyright 2022 <Votre nom et code permanent>
+# Copyright 2022 <JDAM21069406>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
